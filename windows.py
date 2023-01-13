3d5b5954aeab5f8fc2d8da2de2c9fbac6e1fe6f2
Alex Shen <shen.alexander@gmail.com>
2023-01-13 02:53:54 -0500

    Added compiler for Powershell

---
+++ windows.py
@@ -0,0 +1,24 @@
+import os
+
+if os.name != 'nt':
+    raise Exception('This should only be run on Windows')
+
+with open('.aliases', 'r') as f:
+    aliases = f.read().split('\n')
+
+profile = os.getenv(
+    'HOME') + '\\Documents\\WindowsPowerShell\\Microsoft.PowerShell_profile.ps1'
+os.makedirs(os.path.dirname(profile), exist_ok=True)
+with open(profile, 'w+') as f:
+    # These are already Windows aliases but I don't care what they're supposed to be
+    f.write(f'del alias:gc -Force\n')
+    f.write(f'del alias:gcm -Force\n')
+    f.write(f'del alias:gp -Force\n')
+    for alias in aliases:
+        if alias[0:5] == 'alias' and '<' not in alias:
+            # The alias name will always start at index 6
+            name = alias[6:alias.index('=')]
+            value = alias[alias.index('=') + 2:-1]
+
+            f.write(f'Function {name}_ {{{value}}}\n')
+            f.write(f'Set-Alias -Name {name} -Value {name}_\n\n')
